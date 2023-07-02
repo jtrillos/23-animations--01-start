cfb88f4ab5b62aca592dfea79258df57355f3cec
Jaime M Trillos <jaimemanueltrillos@gmail.com>
2023-07-02 15:46:27 +0200

    animation timings

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,9 +3,14 @@ import React from "react";
 import "./Modal.css";
 import { Transition } from "react-transition-group";
 
+const animationTiming = {
+  enter: 400,
+  exit: 1000,
+};
+
 const modal = (props) => {
   return (
-    <Transition in={props.show} timeout={300} mountOnEnter unmountOnExit>
+    <Transition in={props.show} timeout={animationTiming} mountOnEnter unmountOnExit>
       {(state) => {
         const cssClasses = [
           "Modal",
